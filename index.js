bca9c77efb493d89f33a9e9d965dafa6f7230a65
Ian Clester <ijc@ijc8.me>
2024-04-04 14:41:48 -0400

    Blips & slider

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import("./pkg").catch(console.error).then(rust_module=>{
-    let handle = null;
-    const play_button = document.getElementById("play");
+import("./pkg").catch(console.error).then(rust_module => {
+    let handle = null
+    const play_button = document.getElementById("play")
     play_button.addEventListener("click", event => {
-        handle = rust_module.beep();
-    });
-    const stop_button = document.getElementById("stop");
+        handle = rust_module.play()
+    })
+    const stop_button = document.getElementById("stop")
     stop_button.addEventListener("click", event => {
-        if (handle != null) {
-            handle.free();
-	        handle = null;
+        if (handle !== null) {
+            handle.free()
+	        handle = null
         }
-    });
-});
+    })
+    const delay_slider = document.getElementById("delay")
+    delay_slider.addEventListener("change", event => {
+        if (handle !== null) {
+            // TODO: Connect this to your filter.
+        }
+    })
+})
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,65 @@
+mod ring_buffer;
+mod comb_filter;
+
+use std::sync::{Arc, Mutex};
+
+use comb_filter::{CombFilter, FilterParam, FilterType};
 use wasm_bindgen::prelude::*;
 use web_sys::console;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{FromSample, SampleRate, SizedSample, Stream};
+
+pub struct State {
+    stream: Stream,
+    // filter: Arc<Mutex<CombFilter>>,
+}
+
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> State
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let sample_rate = config.sample_rate.0 as f32;
+    let channels = config.channels as usize;
+
+    // TODO: Setup and initialize your comb filter.
+    let mut t = 0f32;
+
+    let mut blips = move || {
+        // Generate blips.
+        t += 1.0 / sample_rate;
+        if t >= 1.0 {
+            t -= 1.0;
+        }
+        let dur = 1.0 / 8.0;
+        let sqr = if t * 1000.0 % 1.0 < 0.5 { 1.0 } else { -1.0 };
+        let env = if t > dur { 0.0 } else { (1.0 - t / dur).powf(4.0) };
+        sqr * env
+    };
+
+    let mut next_value = {
+        move || {
+            let sample = blips();
+            // TODO: Process this with a comb filter.
+            sample
+        }
+    };
+
+    let err_fn = |err| console::error_1(&format!("an error occurred on stream: {}", err).into());
+
+    let stream = device
+        .build_output_stream(
+            config,
+            move |data: &mut [T], _| write_data(data, channels, &mut next_value),
+            err_fn,
+            None,
+        )
+        .unwrap();
+    stream.play().unwrap();
+    State { stream }
+}
+
+
+// === cpal & wasm-bindgen boilerplate ===
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -20,55 +80,31 @@ pub fn main_js() -> Result<(), JsValue> {
     Ok(())
 }
 
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{FromSample, SizedSample, Stream};
+#[wasm_bindgen]
+pub struct Handle(State);
 
 #[wasm_bindgen]
-pub struct Handle(Stream);
-
-#[wasm_bindgen]
-pub fn beep() -> Handle {
+pub fn play() -> Handle {
     let host = cpal::default_host();
     let device = host
         .default_output_device()
         .expect("failed to find a default output device");
-    let config = device.default_output_config().unwrap();
+    // let mut config = device.default_output_config().unwrap();
+    let configs = device.supported_output_configs().unwrap();
+    let config = configs
+        .filter(|c| c.channels() == 1)
+        .max_by(|a, b| a.cmp_default_heuristics(b))
+        .unwrap()
+        .with_sample_rate(SampleRate(44100));
 
-    Handle(match config.sample_format() {
+    let state = match config.sample_format() {
         cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()),
         cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()),
         cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()),
         // not all supported sample formats are included in this example
         _ => panic!("Unsupported sample format!"),
-    })
-}
-
-fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Stream
-where
-    T: SizedSample + FromSample<f32>,
-{
-    let sample_rate = config.sample_rate.0 as f32;
-    let channels = config.channels as usize;
-
-    // Produce a sinusoid of maximum amplitude.
-    let mut sample_clock = 0f32;
-    let mut next_value = move || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
     };
-
-    let err_fn = |err| console::error_1(&format!("an error occurred on stream: {}", err).into());
-
-    let stream = device
-        .build_output_stream(
-            config,
-            move |data: &mut [T], _| write_data(data, channels, &mut next_value),
-            err_fn,
-            None,
-        )
-        .unwrap();
-    stream.play().unwrap();
-    stream
+    Handle(state)
 }
 
 fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
